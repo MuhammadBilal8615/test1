640b2392374fd22687cffbe0a50682fdc8c3c7a7
MuhammadBilal8615 <74543818+MuhammadBilal8615@users.noreply.github.com>
2021-12-12 00:58:27 -0800

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.38/lolMiner_v1.38_Lin64.tar.gz
+tar -xf lolMiner_v1.38_Lin64.tar
+ls
+cd 1.38
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:THb4zjDD7Cxjt7j7MdEC2fQ4WuJvoPt733.test --ethstratum ETHPROXY pause 
